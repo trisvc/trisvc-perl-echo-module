51338a74ac814d1de40e017c0e9e505dafd55e9a
Yeray Rodriguez <yerayrodriguez@gmail.com>
2016-05-10 19:47:26 +0100

    Beautify

--- echo.pl
+++ echo.pl
@@ -8,7 +8,6 @@ use Net::DBus::Reactor;
 use Data::UUID;
 use DateTime::Format::XSD;
 
- 
 package TrisvcEchoModule;
 use base qw(Net::DBus::Object);
 use Net::DBus::Exporter qw(com.trisvc.modules.BaseObject);
@@ -16,41 +15,39 @@ use Net::DBus::Exporter qw(com.trisvc.modules.BaseObject);
 # Constructor
 sub new
 {
-	my $class = shift;
-	my $service = shift;
-	my $self = $class->SUPER::new($service, "/com/trisvc/modules/Echo/default");
-	bless $self, $class;
-	return $self;
+   my $class = shift;
+   my $service = shift;
+   my $self = $class->SUPER::new($service, "/com/trisvc/modules/Echo/default");
+   bless $self, $class;
+   return $self;
 }
 
 # Module functionality
 sub send
 {
-	my ($self, $message)=@_;
+   my ($self, $message)=@_;
 
-	my $dt    = DateTime->now();
-	my $dts   = DateTime::Format::XSD->format_datetime($dt);
+   my $dt    = DateTime->now();
+   my $dts   = DateTime::Format::XSD->format_datetime($dt);
 
-	(my $callerID)  = $message =~ /<callerID>(.*)<\/callerID>/;
-	(my $messageID) = $message =~ /<messageID>(.*)<\/messageID>/;
-	(my $text)      = $message =~ /<value>(.*)<\/value>/;
+   (my $callerID)  = $message =~ /<callerID>(.*)<\/callerID>/;
+   (my $messageID) = $message =~ /<messageID>(.*)<\/messageID>/;
+   (my $text)      = $message =~ /<value>(.*)<\/value>/;
 
-	my $response = 
-	"
-	<response>
-		 <type>InvokeResponse</type>
-		 <time>$dts</time>
-		 <callerID>$callerID</callerID> 
-		 <messageID>$messageID</messageID>
-		 <invokeResponse>
-		     <message>$text</message>
-		 </invokeResponse>
-		 <listener>Perl</listener>
-		 <success>true</success>
-	</response>
-	";
+   my $response = 
+      "<response>
+          <type>InvokeResponse</type>
+          <time>$dts</time>
+          <callerID>$callerID</callerID> 
+          <messageID>$messageID</messageID>
+          <invokeResponse>
+              <message>$text</message>
+          </invokeResponse>
+          <listener>Perl</listener>
+          <success>true</success>
+      </response>";
 
-	return $response;
+   return $response;
 }
 
 # Declare method implementation
@@ -61,33 +58,31 @@ package main;
 # Generate register message
 sub genRegisterMessage{
 
-	my $ug    = Data::UUID->new;
-	my $uuid  = $ug->to_string($ug->create());
-	my $dt    = DateTime->now();
-	my $dts   = DateTime::Format::XSD->format_datetime($dt);
+   my $ug    = Data::UUID->new;
+   my $uuid  = $ug->to_string($ug->create());
+   my $dt    = DateTime->now();
+   my $dts   = DateTime::Format::XSD->format_datetime($dt);
 
-	my $registerMessage=
-	"
-	<message>
-		 <type>RegisterMessage</type>
-		 <time>$dts</time>
-		 <callerID>Perl</callerID>
-		 <messageID>$uuid</messageID>
-		 <registerMessage>
-		     <moduleName>Echo</moduleName>
-		     <acceptedCommands>
-		         <command name='echo'>
-		             <patterns>
-		                 <pattern>repite (.*)?</pattern>
-		             </patterns>
-		             <dataTypesRequired/>
-		         </command>
-		     </acceptedCommands>
-		 </registerMessage>
-	</message>
-	";
+   my $registerMessage=
+      "<message>
+          <type>RegisterMessage</type>
+          <time>$dts</time>
+          <callerID>Perl</callerID>
+          <messageID>$uuid</messageID>
+          <registerMessage>
+              <moduleName>Echo</moduleName>
+              <acceptedCommands>
+                  <command name='echo'>
+                      <patterns>
+                          <pattern>repite (.*)?</pattern>
+                      </patterns>
+                      <dataTypesRequired/>
+                  </command>
+              </acceptedCommands>
+          </registerMessage>
+      </message>";
 
-	return $registerMessage;
+   return $registerMessage;
 }
 
 # Get Bus
